1ac77e7165cfb7ff266ec2ea3e1194edcfa5d9d1
Horacio Duran <horacio.duran@gmail.com>
2022-04-26 22:27:17 +0200

    Added the bases for Cleanup, and Snapshot compare

--- asserts.go
+++ asserts.go
@@ -1,11 +1,14 @@
 package expect
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/url"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"sync"
 	"testing"
@@ -42,20 +45,100 @@ func init() {
 
 const snapshotFilePerm = 0755
 
-func registerTestName(testName string) {
-	registerNameMutex.Lock()
-	defer registerNameMutex.Unlock()
-	registeredName[testName] = true
+type ErrRepeated struct {
+	repeatedSnapshot string
 }
 
+func (e *ErrRepeated) Error() string {
+	return fmt.Sprintf("expectation %q is already set", e.repeatedSnapshot)
+}
+
+func (e *ErrRepeated) Is(err error) bool {
+	_, isErr := err.(*ErrRepeated)
+	return isErr
+}
+
+func registerTestName(testName string) error {
+	registerNameMutex.Lock()
+	defer registerNameMutex.Unlock()
+	if registeredName[testName] {
+		return &ErrRepeated{repeatedSnapshot: testName}
+	}
+	registeredName[testName] = true
+	return nil
+}
+
+type fileHeader struct {
+	OS        string `json:"os"`
+	LimitToOS bool   `json:"limit_to_os"`
+}
+
+func (f *fileHeader) dump() ([]byte, error) {
+	return json.Marshal(f)
+}
+
+func (f *fileHeader) load(h []byte) error {
+	return json.Unmarshal(h, f)
+}
+
+func (f *fileHeader) considerForCleanup() bool {
+	return (f.LimitToOS && runtime.GOOS == f.OS) || !f.LimitToOS
+}
+
+type fileContents struct {
+	header *fileHeader
+	body   []byte
+}
+
+var headerSep = []byte("\n\n")
+
+func (f *fileContents) dump(fileName string) error {
+	h, err := f.header.dump()
+	if err != nil {
+		return fmt.Errorf("dumping header %w", err)
+	}
+	return os.WriteFile(fileName,
+		append(h, append(headerSep, f.body...)...),
+		snapshotFilePerm)
+}
+
+func (f *fileContents) load(fileName string) error {
+	fContent, err := os.ReadFile(fileName)
+	if err != nil {
+		return fmt.Errorf("reading file for loading: %w", err)
+	}
+	sep := bytes.Index(fContent, headerSep)
+	if sep == -1 {
+		return fmt.Errorf("malformed expectation, cannot find separator")
+	}
+	f.header = &fileHeader{}
+	f.header.load(fContent[:sep])
+	f.body = fContent[sep+len(headerSep):]
+	return nil
+}
 func AsSnapshot(t *testing.T, name string, comparable Comparable) {
+	asSnapshot(t, name, comparable, false)
+}
+
+func AsOSDependentSnapshot(t *testing.T, name string, comparable Comparable) {
+	asSnapshot(t, name, comparable, true)
+}
+
+func asSnapshot(t *testing.T, name string, comparable Comparable, limitOS bool) {
 	pathName := url.PathEscape(name)
-	registerTestName(pathName)
+	if err := registerTestName(pathName); err == nil {
+		t.Error(fmt.Errorf("setting new expectation: %w", err))
+	}
 	snapshotFilePath := filepath.Join(snapShotDir, pathName)
 	if currentRunArgs != nil && currentRunArgs.shouldUpdate {
-		if err := os.WriteFile(snapshotFilePath, comparable.Dump(), snapshotFilePerm); err != nil {
+		fc := fileContents{
+			header: &fileHeader{OS: runtime.GOOS, LimitToOS: limitOS},
+			body:   comparable.Dump(),
+		}
+		if err := fc.dump(snapshotFilePath); err != nil {
 			panic(err)
 		}
+
 		// we just updated the snapshot, makes no sense to compare
 		return
 	}
@@ -79,4 +162,35 @@ func AsSnapshot(t *testing.T, name string, comparable Comparable) {
 	t.Logf("found a difference between expectation and result on test %q, difference follows:", name)
 	t.Log(diff)
 	t.FailNow()
+}
+
+func Cleanup() error {
+	registerNameMutex.Lock()
+	defer registerNameMutex.Unlock()
+	if currentRunArgs == nil || (currentRunArgs != nil && !currentRunArgs.shouldCleanup) {
+		return nil
+	}
+	dirContents, err := os.ReadDir(snapShotDir)
+	if err != nil {
+		return fmt.Errorf("reading snapshot directory contents: %w", err)
+	}
+	var deletable []string
+	for _, entry := range dirContents {
+		p := filepath.Join(snapShotDir, entry.Name())
+		fc := &fileContents{}
+		fc.load(p)
+		if !fc.header.considerForCleanup() {
+			continue
+		}
+		if registeredName[entry.Name()] {
+			continue
+		}
+		deletable = append(deletable, p)
+	}
+	for i, d := range deletable {
+		if err := os.Remove(d); err != nil {
+			return fmt.Errorf("deleting stale snapshot, %d were deleted before failure: %w", i, err)
+		}
+	}
+	return nil
 }
