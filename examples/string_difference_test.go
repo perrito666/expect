3d6dd453d4a3cbcd3276b25215018cc1a8d5f5cb
Horacio Duran <horacio.duran@gmail.com>
2023-11-15 11:21:38 +0100

    Update broken tests

--- examples/string_difference_test.go
+++ examples/string_difference_test.go
@@ -10,25 +10,25 @@ import (
 func TestStringSimpleMoreTextFails(t *testing.T) {
 	moreTestThan := `Hello, World
 Hello Universe`
-	c := comparabletypes.StringComparable(moreTestThan)
-	expect.FromSnapshot(t, "comparable has more text than snapshot", &c)
+	c := comparabletypes.NewStringComparable(moreTestThan)
+	expect.FromSnapshot(t, "comparable has more text than snapshot", c)
 }
 
 func TestStringSimpleLessTextFails(t *testing.T) {
 	moreTestThan := `Hello World`
-	c := comparabletypes.StringComparable(moreTestThan)
-	expect.FromSnapshot(t, "comparable has less text than snapshot", &c)
+	c := comparabletypes.NewStringComparable(moreTestThan)
+	expect.FromSnapshot(t, "comparable has less text than snapshot", c)
 }
 
 func TestPrettyStringSimpleMoreTextFails(t *testing.T) {
 	moreTestThan := `Hello, World
 Hello Universe`
-	c := comparabletypes.PrettyStringComparable{StringComparable: comparabletypes.StringComparable(moreTestThan)}
-	expect.FromSnapshot(t, "pretty comparable has more text than snapshot", &c)
+	c := comparabletypes.NewPrettyStringComparable(moreTestThan)
+	expect.FromSnapshot(t, "pretty comparable has more text than snapshot", c)
 }
 
 func TestPrettyStringSimpleLessTextFails(t *testing.T) {
 	moreTestThan := `Hello World`
-	c := comparabletypes.PrettyStringComparable{StringComparable: comparabletypes.StringComparable(moreTestThan)}
-	expect.FromSnapshot(t, "pretty comparable has less text than snapshot", &c)
+	c := comparabletypes.NewPrettyStringComparable(moreTestThan)
+	expect.FromSnapshot(t, "pretty comparable has less text than snapshot", c)
 }
